f97dbee5d81fc1eace2bf678b6f796d676bc49b6
Leah Chen <leeewchen@gmail.com>
2022-05-10 15:10:52 -0700

    event listener with second fetch with html and css

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('https://www.themealdb.com/api/json/v1/1/categories.php').then(resp => resp.json()).then(data => {
-        category(data.categories)
+    fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+    .then(resp => resp.json())
+    .then(data => {
+        category(data.categories) 
     })
+//selects our top menu. iterates through our categories- and lists them out.     
     const topMenu = document.querySelector('#top_menu')
     console.log(topMenu)
 
@@ -11,13 +15,47 @@ document.addEventListener('DOMContentLoaded', () => {
             catItem.textContent = element.strCategory
 
             catItem.addEventListener('click', (e) => {
-                // will need another fetch for category-clicked recipes
-                // when clicked display image of dish and dish title
+                // when category clicked display image of dish and dish title
+                fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=' + element.strCategory) // our category goes in here - need to change
+                .then(resp => resp.json())
+                .then(recipeName => {
+                    meals(recipeName.meals)
+                    console.log(recipeName)
+
+                })
             })
-
             //(somewhere) when click an option in the category, display recipe image, name, and instructions
-
-            topMenu.append(catItem)
+            topMenu.append(catItem)        
         });
     }
+
+    const meals= (RecipeArray) => {
+        const recipeImageContainer= document.querySelector('#category_options')
+        recipeImageContainer.innerHTML= ""
+
+        RecipeArray.forEach(mealItem => {
+            
+            const recipeImage= document.createElement('img')
+            recipeImage.src= mealItem.strMealThumb
+
+            const recipeTitle= document.createElement('h2')
+            recipeTitle.textContent= mealItem.strMeal
+
+            recipeImageContainer.append(recipeImage, recipeTitle)
+
+            // recipeImage.addEventListener('click', (e) => {
+            //     // when category clicked display image of dish and dish title
+            //     fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + mealItem.idMeal) // our category goes in here - need to change
+            //     .then(resp => resp.json())
+            //     .then(recipeName => {
+            //         (recipeName.idMeal)
+            //         console.log(recipeName)
+
+            //     })
+            // })
+        })
+    }
 })
+
+
+
